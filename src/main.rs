370d0b2ff8ac3374cf2206d77ea521add8e2e3ee
0xMRTT <0xMRTT@tuta.io>
2022-06-11 18:46:07 +0200

    Try to add int input

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use locales::t;
 use console::style;
 
 use rand::Rng;
+use requestty::Question;
 
 fn main() {
     let lang = "en";
@@ -12,9 +13,23 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     
 
-    loop {
+    //loop {
+
+        let user_number = Question::int("number")
+        .message("Enter your number?")
+        .validate(|number, previous_answers| {
+            if number > 0 && number <= 100 {
+                Ok(())
+            } else {
+                Err(format!("Number must be higher than 0 and lower than 100 (current number {} )", number))
+            }
+        })
+        .build();
+        println!("{:#?}", requestty::prompt_one(user_number));
+        println!("{:#?}", number)
         
-    }
+
+    //}
 
     
     //println!(!("out.hello_world", lang);
