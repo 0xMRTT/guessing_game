49171a4906ecd7d89d954f5327cac5e2ce2f9cba
0xMRTT <0xMRTT@tuta.io>
2022-06-11 18:20:05 +0200

    Add console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use locales::t;
+use std::thread;
+use std::time::Duration;
+
+use console::Term;
+use console::style;
+
 
 fn main() {
     let lang = "en";
-    println!("{}", t!("out.hello_world", lang));
+
+
+    let term = Term::stdout();
+    term.write_line(&t!("out.hello_world", lang).to_string());
+    thread::sleep(Duration::from_millis(2000));
+    term.clear_line();
+
+    println!("This is {} neat", style("quite").blue());
+    
 }
