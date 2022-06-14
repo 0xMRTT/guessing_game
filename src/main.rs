687fb9d93332d2f5dbef1e1e6d8d4c3618ec3a84
0xMRTT <0xMRTT@tuta.io>
2022-06-14 19:11:18 +0200

    Reformat code to for asking player after about

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,83 @@ fn main() {
         match continue_game {
             Ok(choice) => {
                 match choice {
-                    "Start a game" => println!(
+                    "Start a game" => {println!(
                         "Start a new game {}",
-                        emojicon.get_by_name("game").unwrap().collect::<Vec<_>>()[0]
-                    ),
+                        emojicon.get_by_name("game").unwrap().collect::<Vec<_>>()[0]);
+
+                        println!(
+                            "Generating the secret number ... {}",
+                            emojicon.get_by_name("key").unwrap().collect::<Vec<_>>()[0]
+                        );
+                        let secret_number = rand::thread_rng().gen_range(1..101);
+                        let mut try_user = 0;
+                        loop {
+                            let user_number = CustomType::<u32>::new("Whats your number ?")
+                                .with_formatter(&|i| format!("{:.2}", i))
+                                .with_error_message("Please type a valid number")
+                                .with_help_message(format!("Write a number ({})", try_user).as_str())
+                                .with_placeholder("number")
+                                .prompt();
+                
+                            match user_number {
+                                Ok(_) => println!(
+                                    "Check if match.... {}",
+                                    emojicon.get_by_name("fire").unwrap().collect::<Vec<_>>()[0]
+                                ),
+                                Err(_) => println!(
+                                    "Good bye {}",
+                                    emojicon.get_by_name("wave").unwrap().collect::<Vec<_>>()[0]
+                                ),
+                            }
+                
+                            try_user = try_user + 1;
+                
+                            match user_number.unwrap().cmp(&secret_number) {
+                                Ordering::Less => {
+                                    println!(
+                                        "It's higher! {}",
+                                        emojicon
+                                            .get_by_name("thumbsup")
+                                            .unwrap()
+                                            .collect::<Vec<_>>()[0]
+                                    );
+                                }
+                                Ordering::Greater => {
+                                    println!(
+                                        "It's lower {}",
+                                        emojicon
+                                            .get_by_name("thumbsdown")
+                                            .unwrap()
+                                            .collect::<Vec<_>>()[0]
+                                    );
+                                }
+                                Ordering::Equal => {
+                                    println!(
+                                        "You won in {}! {}",
+                                        try_user,
+                                        emojicon.get_by_name("trophy").unwrap().collect::<Vec<_>>()[0]
+                                    );
+                
+                                    if try_user < best_try {
+                                        best_try = try_user;
+                                        println!("It's your new goals to beat. Good luck!");
+                                    }
+                                    score = score + 1;
+                                    break;
+                                }
+                            }
+                
+                            
+                        }
+                
+                        println!(
+                            "You have {} {}",
+                            score,
+                            emojicon.get_by_name("trophy").unwrap().collect::<Vec<_>>()[0]
+                        );
+                
+                        // END: GAME
+                    },
                     "Quit" => {
                         println!("Your best score is {}", best_try);
                         break;
@@ -59,78 +132,7 @@ fn main() {
             Err(_) => println!("There was an error, please try again"),
         }
 
-        println!(
-            "Generating the secret number ... {}",
-            emojicon.get_by_name("key").unwrap().collect::<Vec<_>>()[0]
-        );
-        let secret_number = rand::thread_rng().gen_range(1..101);
-        let mut try_user = 0;
-        loop {
-            let user_number = CustomType::<u32>::new("Whats your number ?")
-                .with_formatter(&|i| format!("{:.2}", i))
-                .with_error_message("Please type a valid number")
-                .with_help_message(format!("Write a number ({})", try_user).as_str())
-                .with_placeholder("number")
-                .prompt();
-
-            match user_number {
-                Ok(_) => println!(
-                    "Check if match.... {}",
-                    emojicon.get_by_name("fire").unwrap().collect::<Vec<_>>()[0]
-                ),
-                Err(_) => println!(
-                    "Good bye {}",
-                    emojicon.get_by_name("wave").unwrap().collect::<Vec<_>>()[0]
-                ),
-            }
-
-            try_user = try_user + 1;
-
-            match user_number.unwrap().cmp(&secret_number) {
-                Ordering::Less => {
-                    println!(
-                        "It's higher! {}",
-                        emojicon
-                            .get_by_name("thumbsup")
-                            .unwrap()
-                            .collect::<Vec<_>>()[0]
-                    );
-                }
-                Ordering::Greater => {
-                    println!(
-                        "It's lower {}",
-                        emojicon
-                            .get_by_name("thumbsdown")
-                            .unwrap()
-                            .collect::<Vec<_>>()[0]
-                    );
-                }
-                Ordering::Equal => {
-                    println!(
-                        "You won in {}! {}",
-                        try_user,
-                        emojicon.get_by_name("trophy").unwrap().collect::<Vec<_>>()[0]
-                    );
-
-                    if try_user < best_try {
-                        best_try = try_user;
-                        println!("It's your new goals to beat. Good luck!");
-                    }
-                    score = score + 1;
-                    break;
-                }
-            }
-
-            
-        }
-
-        println!(
-            "You have {} {}",
-            score,
-            emojicon.get_by_name("trophy").unwrap().collect::<Vec<_>>()[0]
-        );
-
-        // END: GAME
+        
 
         
     }
